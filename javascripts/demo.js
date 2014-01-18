285137e76a825cfdcfd706b94358aa05e264c536
luanrafael <luan-g8@hotmail.com>
2014-01-18 14:40:00 -0200

    demo.js

--- javascripts/demo.js
+++ javascripts/demo.js
@@ -1,4 +1,16 @@
 
+window.onload = function (){
+	if(JLogam.setup()){
+		document.getElementById("bolinha").style.background = 'blue';
+	};
+	JLogam.on(
+		"cheers",
+		function (){
+			twitte();
+		}
+	);
+}
+
 function cliSimulate(){
 	var command = document.getElementById("clitext").value;
 	if(command == "#JLogam"){
@@ -11,12 +23,9 @@ function cliSimulate(){
 
 
 
-
-
-
-JLogam.set(function (){
-	if(supportsVibrate){
-		navigator.vibrate(1000);
-	}
+function twitte(){
+    console.log("TIM... TIM...");
+    document.getElementById("bolinha").style.background = 'blue';
     window.location.assign("https://twitter.com/intent/tweet?hashtags=JLogam%2C&original_referer=http%3A%2F%2Flocalhost%3A8080%2FJLogam%2Fdemo%2Fcheers.html&related=lrpinheiroo&text=JLogam%20-%20JavaScript%20Libary%20for%20Open%20Gestures%20Acessibility%20Mobile&tw_p=tweetbutton");
-});
+	return false;
+}
