2d46cf186acd2b799cde27091db85604b943585b
luanrafael <luan-g8@hotmail.com>
2014-02-22 17:27:52 -0300

    Gestures calibration

--- javascripts/directions.js
+++ javascripts/directions.js
@@ -1,15 +1,24 @@
 window.onload = function () {
-	if(JLogam.setup()){
-		JLogam.on("front",function(){ setDirection("front"); });
-		JLogam.on("back",function(){ setDirection("back"); });
-		JLogam.on("left",function(){ setDirection("left"); });
-		JLogam.on("right",function(){ setDirection("right"); });
-	}
-}
+    if (JLogam.setup()) {
+        azul();
+        JLogam.on("front", function () {
+            setDirection("front");
+        });
+        JLogam.on("back", function () {
+            setDirection("back");
+        });
+        JLogam.on("left", function () {
+            setDirection("left");
+        });
+        JLogam.on("right", function () {
+            setDirection("right");
+        });
+    }
+};
 
 
 
-function setDirection(direction){
-	document.getElementById("imgdirection").setAttribute("src","../images/" + direction + '.png');
-	return true;
+function setDirection(direction) {
+    document.getElementById("imgdirection").setAttribute("src", "../images/" + direction + '.png');
+    return true;
 }
--- javascripts/yes_no.js
+++ javascripts/yes_no.js
@@ -1,55 +1,60 @@
-window.onload = function (){
-	document.querySelector('#cards').addEventListener('click', function (e) {
-		if(e.target.id == "cards"){
-			return false;
-		}
-	    if (document.getElementById("card_selecionado") !== null) {
-	        document.getElementById("card_selecionado").style.background = 'white';
-	        document.getElementById("card_selecionado").setAttribute("id", this.getAttribute("data-pos"));
-	    }
-	    e.target.setAttribute("id", "card_selecionado");
-	    document.getElementById("card_selecionado").style.background = 'lightblue';
-	});
+window.onload = function () {
+    document.querySelector('#cards').addEventListener('click', function (e) {
+        if (e.target.id == "cards") {
+            return false;
+        }
+        if (document.getElementById("card_selecionado") !== null) {
+            document.getElementById("card_selecionado").style.background = 'white';
+            document.getElementById("card_selecionado").setAttribute("id", this.getAttribute("data-pos"));
+        }
+        e.target.setAttribute("id", "card_selecionado");
+        document.getElementById("card_selecionado").style.background = 'lightblue';
+    });
 
-	if(JLogam.setup()){
-		document.getElementById("bolinha").style.background = 'blue';
-		JLogam.on("yes", function(){openCard();});
-		JLogam.on("no", function(){discartCard();});
-	}
+    if (JLogam.setup()) {
+        document.getElementById("bolinha").style.background = 'blue';
+        JLogam.on("yes", function () {
+            openCard();
+        });
+        JLogam.on("no", function () {
+            discartCard();
+        });
+    }
+};
+
+
+function openCard() {
+    var card = document.getElementById("card_selecionado");
+    if (card !== null) {
+        var arg = card.getAttribute("data-selected");
+        switch (arg) {
+            case "CLI":
+                alert("CLI - COMMAND LINE INTERFACE");
+                break;
+
+            case "GUI":
+                alert("GUI - GRAPHICAL USER INTERFACE");
+                break;
+
+            case "NUI":
+                window.location.assign("https://twitter.com/intent/tweet?hashtags=JLogam%2C&original_referer=http%3A%2F%2Flocalhost%3A8080%2FJLogam%2Fdemo%2Fcheers.html&related=lrpinheiroo&text=JLogam%20-%20JavaScript%20Libary%20for%20Open%20Gestures%20Acessibility%20Mobile&tw_p=tweetbutton");
+                break;
+        }
+        return true;
+    }
+    alert("Select one card!");
+    return false;
 }
 
 
-function openCard(){
-	var card = document.getElementById("card_selecionado");
-	if(card !== null){
-		var arg = card.getAttribute("data-selected");
-		switch (arg){
-			case "CLI":
-				alert("CLI - COMMAND LINE INTERFACE");
-				break;
-
-			case "GUI":
-				alert("GUI - GRAPHICAL USER INTERFACE")
-				break;
-
-			case "NUI":
-				window.location.assign("https://twitter.com/intent/tweet?hashtags=JLogam%2C&original_referer=http%3A%2F%2Flocalhost%3A8080%2FJLogam%2Fdemo%2Fcheers.html&related=lrpinheiroo&text=JLogam%20-%20JavaScript%20Libary%20for%20Open%20Gestures%20Acessibility%20Mobile&tw_p=tweetbutton");
-				break;
-		}
-		return true;
-	}
-	alert("Select one card!");
-	return false;
+function discartCard() {
+    var card = document.getElementById("card_selecionado");
+    if (card !== null) {
+        document.getElementById("cards").removeChild(card);
+        vibrar();
+        alert("Card discarded");
+        return true;
+    }
+    alert("Select one card!");
+    return false;
 }
-
-function discartCard(){
-	var card = document.getElementById("card_selecionado");
-	if(card !== null){
-		document.getElementById("cards").removeChild(card);
-		return true;
-	}
-	alert("Select one card!");
-	return false;
-}
-
-
